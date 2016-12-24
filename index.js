ad807967cb34d4930668362cead71ed4a8f8304d
David Anson <david@dlaa.me>
2016-12-23 23:12:48 -0800

    Add React for rendering.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 'use strict';
 
-document.write('Hello world!');
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+	React.createElement('div', null, 'Hello world!'),
+	document.getElementById('root'));
