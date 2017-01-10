87de188565122c8b635c5e3bdaf425c2c006975a
David Anson <david@dlaa.me>
2017-01-09 22:45:25 -0800

    Decode JPEG thumbnail image, use as preview during load.

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 const {ipcRenderer, remote} = require('electron');
 const fs = require('fs');
 const path = require('path');
+const Datauri = require('datauri');
 const pify = require('pify');
 const React = require('react');
 const ReactDOM = require('react-dom');
 const ipc = require('./ipc.js');
 
+const fsReaddir = pify(fs.readdir);
 const dialog = remote.dialog;
 const getExifIpc = ipc.createClient(ipcRenderer, 'getExif');
-const imageRe = /\.(bmp|gif|png|jpeg|jpg|jxr|tif|tiff|webp)$/i;
+const imageRe = /\.(bmp|gif|png|jpeg|jpg|jxr|webp)$/i;
 const exifImageOrientationMap = {
 	1: 'rotate-none',
 	3: 'rotate-flip',
@@ -30,21 +32,36 @@ class Picture extends React.Component {
 	render() {
 		if (this.state.stage === 'loading') {
 			getExifIpc(this.props.file, exif => {
-				const orientation = exif && exif.image && exif.image.Orientation && exifImageOrientationMap[exif.image.Orientation];
+				const orientation = exifImageOrientationMap[exif.orientation];
+				let thumbnail = null;
+				if (exif.thumbnail) {
+					const datauri = new Datauri();
+					datauri.format('.jpg', exif.thumbnail);
+					thumbnail = datauri.content;
+				}
 				this.setState({
 					stage: 'preview',
-					orientation
+					orientation,
+					thumbnail
 				});
 			});
 		}
 		const children = [];
 		const pushImage = props => {
+			props.title = path.basename(this.props.file);
 			children.push(React.createElement('img', props));
 		};
 		if (this.state.stage === 'loading') {
 			pushImage({src: 'waiting.svg'});
 		} else if (this.state.stage === 'preview') {
-			pushImage({src: 'waiting.svg'});
+			if (this.state.thumbnail) {
+				pushImage({
+					src: this.state.thumbnail,
+					className: this.state.orientation
+				});
+			} else {
+				pushImage({src: 'waiting.svg'});
+			}
 			pushImage({
 				src: this.props.file,
 				className: 'hidden',
@@ -58,7 +75,6 @@ class Picture extends React.Component {
 		} else if (this.state.stage === 'loaded') {
 			pushImage({
 				src: this.props.file,
-				title: path.basename(this.props.file),
 				className: this.state.orientation
 			});
 		} else if (this.state.stage === 'error') {
@@ -113,7 +129,7 @@ class Page extends React.Component {
 			.then(directories => {
 				if (directories) {
 					const directory = directories[0];
-					return pify(fs.readdir)(directory)
+					return fsReaddir(directory)
 						.then(allFiles => {
 							const files = allFiles
 								.filter(file => file.match(imageRe))
--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const {app, BrowserWindow, ipcMain} = require('electron');
+const fs = require('fs');
 const path = require('path');
 const url = require('url');
-const {ExifImage} = require('exif');
+const fastExif = require('fast-exif');
+const pify = require('pify');
 const ipc = require('./ipc.js');
 const packageJson = require('./package.json');
+
+const fsOpen = pify(fs.open);
+const fsRead = pify(fs.read);
+const fsClose = pify(fs.close);
 
 let win;
 
@@ -41,23 +47,50 @@ app.on('activate', () => {
 	}
 });
 
-function getExif(file) {
-	return new Promise((resolve, reject) => {
-		// eslint-disable-next-line no-new
-		new ExifImage({image: file}, (error, exif) => {
-			if (error) {
-				reject(error);
-			} else {
-				resolve(exif);
-			}
-		});
-	});
-}
-
 ipc.createServer(ipcMain, 'getExif', (arg, reply) => {
-	getExif(arg)
+	fastExif.read(arg)
 		.catch(() => {
 			// Transform errors to empty exif data
 		})
-		.then(reply);
+		.then(exif => {
+			if (exif && exif.thumbnail && exif.thumbnail.ThumbnailOffset && exif.thumbnail.ThumbnailLength) {
+				return fsOpen(arg, 'r')
+					.then(fd => {
+						const thumbnailOffset = exif.thumbnail.ThumbnailOffset;
+						const thumbnailLength = exif.thumbnail.ThumbnailLength;
+						const buffer = new Buffer(thumbnailLength);
+						const exifOffset = 12; // sizeof(JPEG header) + sizeof(APP1 header) + sizeof(Exif header)
+						return fsRead(fd, buffer, 0, thumbnailLength, exifOffset + thumbnailOffset)
+							.then(() => {
+								if ((thumbnailLength >= 4) &&
+									(buffer[0] === 0xff) && (buffer[1] === 0xd8) &&
+									(buffer[thumbnailLength - 2] === 0xff) && (buffer[thumbnailLength - 1] === 0xd9)) {
+									exif.thumbnail.buffer = buffer;
+								}
+							})
+							.catch(() => {
+								// Ignore any read errors
+							})
+							.then(() => {
+								return fd;
+							});
+					})
+					.then(fd => {
+						return fsClose(fd);
+					})
+					.catch(() => {
+						// Ignore any thumbnail errors
+					})
+					.then(() => {
+						return exif;
+					});
+			}
+			return exif;
+		})
+		.then(exif => {
+			reply({
+				orientation: exif && exif.image && exif.image.Orientation,
+				thumbnail: exif && exif.thumbnail && exif.thumbnail.buffer
+			});
+		});
 });
